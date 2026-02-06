b86bf1728e868c4dfa113c998d484179525269da
Nicholas Cheng <74994367+Miniongolf@users.noreply.github.com>
2026-02-05 22:09:58 -0500

    feat: add device constructor

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,19 +1,25 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 #include <Arduino.h>
+
 #include <cstdint>
 
 // Drivetrain motor pins
 namespace drive {
-inline uint8_t directionPins[4] = {4, 5, 2, 3};
-inline uint8_t speedPins[2] = {10, 9};
+inline uint8_t DIRECTION_PINS[4] = {4, 5, 2, 3};
+inline uint8_t SPEED_PINS[2] = {10, 9};
 }  // namespace drive
 
 // Color sensor pins
 namespace color {
 inline uint8_t S2_PIN = A5;
-inline uint8_t S3_PIN = 7; 
+inline uint8_t S3_PIN = 7;
 inline uint8_t OUT_PIN = 8;
+}  // namespace color
+
+// Arm servo pins
+namespace servo {
+inline uint8_t SERVO_PIN = 6;
 }
 
-#endif // CONSTANTS_HPP
+#endif  // CONSTANTS_HPP
--- include/devices.hpp
+++ include/devices.hpp
@@ -4,13 +4,15 @@
 #include <ColorSensor.h>
 #include <DDBot.h>
 
+#include <arm.hpp>
 #include <chassis.hpp>
 
 #include "constants.hpp"
 
-inline DDBot drivetrain(drive::directionPins, drive::speedPins);
+inline DDBot drivetrain(drive::DIRECTION_PINS, drive::SPEED_PINS);
 inline ColorSensor colorSensor(color::S2_PIN, color::S3_PIN, color::OUT_PIN);
-
 inline Chassis chassis(drivetrain, colorSensor);
+
+inline Arm arm(servo::SERVO_PIN);
 
 #endif  // DEVICES_HPP
